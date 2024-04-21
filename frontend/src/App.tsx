bd063fbd0f5dce99d861fb9051056bd8b8f55fd0
William Ferreira <williamferreiragt@gmail.com>
2024-04-21 12:50:56 -0300

    Migrated project to TypeScript and added fileoverview description

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
+/*
+ *  AgendeVc - Plataforma de agendamento de serviços.
+ *  Software desenvolvido por Williamberg Ferreira
+ */
+
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
